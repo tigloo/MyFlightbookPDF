ed1dd544feb8bacd01884b135633cdee06a1d21a
Till Gerken <till.gerken@gmail.com>
2015-12-21 22:51:31 +0100

    ..

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask import request
 import tempfile
+import os
+
+# __file__ refers to the file settings.py 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
+APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return app.send_file('mainform.html')
+	#return app.send_file('mainform.html')
+	return 'APP_ROOT says: %s<br/>Config says: %s' % (APP_ROOT, app.Config['root_path'])
 
 @app.route('/compile', methods=['POST'])
 def compile():
