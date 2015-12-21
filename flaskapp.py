5362e5d558be82fc944bd8c9af5d094006058267
Till Gerken <till.gerken@gmail.com>
2015-12-21 22:42:03 +0100

    Fix it!

--- flaskapp.py
+++ flaskapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-	return app.send_from_directory('','mainform.html')
+	return app.send_file('mainform.html')
 
 @app.route('/compile', methods=['POST'])
 def compile():
